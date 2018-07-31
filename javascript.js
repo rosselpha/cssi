ad74286bc2bb2a7070ab199c378ab373eb53683a
rosselpha <RossElpharin@cslab-11.acldomain.local>
2018-07-31 11:41:30 -0400

    new kamma

--- javascript.js
+++ javascript.js
@@ -23,3 +23,33 @@ if(x){
 
 let x1 = 3.2222;
 let y1 = 56.67788;
+
+if(x1 <=y1){
+  console.log(y1)
+}
+
+let num = -32;
+
+if(num <=50 && num >= -50){
+  console.log(true)
+}
+
+//# 7 = 15
+
+for (let i =1; i <= 100; i++){
+  console.log(i)
+}
+
+// #9
+for ( let i = 1 ;i <= 100; i++){
+  if(i != 17 && i != 43 && i != 100){
+    console.log(i)
+  }
+}
+// #10
+for(let i = 99; i>= 1; i--){
+
+  if(i /2 % 1){
+    console.log(i)
+  }
+}
--- lab-js1/app.js
+++ lab-js1/app.js
@@ -1,9 +1,18 @@
-function sendAlert() {
-//  alert("Hello sweeeaa");
-console.log('ccsmo')
+// function sendAlert() {
+// //  alert("Hello sweeeaa");
+// console.log('ccsmo')
+// }
+// const lmfaoo = 0;
+//
+// let lol = 1;
+//
+//  let main = lmfaoo ? lol : 8;
+
+
+
+for(let i = 99; i>= 1; i--){
+
+  if(i /2 % 1){
+    console.log(i)
+  }
 }
-const lmfaoo = 0;
-
-let lol = 1;
-
- let main = lmfaoo ? lol : 8;
