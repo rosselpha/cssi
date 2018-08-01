263da008712daf29c20e58b2376489a2b13bfc47
rosselpha <RossElpharin@cslab-10.acldomain.local>
2018-08-01 16:18:05 -0400

    malib

--- cssi-labs/javaScript/labs/Alarm_Clock/script.js
+++ cssi-labs/javaScript/labs/Alarm_Clock/script.js
@@ -12,4 +12,94 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-console.log("script is running...");
+// console.log("script is running...");
+// function Basic_Alarm(alarmMessage){
+//   return alarmMessage;
+// };
+//
+// console.log(Basic_Alarm("My alarm!"));
+//
+// function MyAlarm(time){
+//   return  `wake up mr west ${time}`;
+// }
+//
+// console.log(MyAlarm('6:00'))
+//
+// function Mom_Alarm(time) {
+//       return `Hey, Mom, wake up! It’s ${time}`;
+// };
+//
+// console.log(Mom_Alarm("04:00"));
+//
+// let person = prompt('what are u');
+// let time = prompt('what u need to wake u');
+//
+// function Family_Alarm(time, person){
+//   if(person == "mom"){
+//     return `Hey, Mom, wake up! It’s ${time}`;
+//
+//   }else if (person == "me") {
+//     return  `wake up mr west ${time}`;
+//   }else {
+//     return "u need to make an alarm"
+//   }
+// }
+//
+//
+// console.log(Family_Alarm(time,person));
+
+// let msg = prompt('what is your wake up msg');
+
+// function Important_Alarm (msg) {
+//   msg = prompt('what is your wake up msg');
+//   return msg.toUpperCase();
+// }
+// console.log(Important_Alarm())
+
+
+// function Snooze_Alarm (time) {
+//     time = parseInt(prompt('how long do u want to snooze --- in minutes'));
+//     let times2 = time * 20000
+//     setInterval( Snooze_Alarm(), times2);
+//
+// }
+// Snooze_Alarm();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
